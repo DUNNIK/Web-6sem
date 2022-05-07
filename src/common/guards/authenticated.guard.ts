e055f6b39fe153b3719c80222fe43cad8944768a
DUNNIK <dns.56@mail.ru>
2022-05-07 14:31:40 +0300

    edit: remove logic from controllers edit: dto, entities

--- src/common/guards/authenticated.guard.ts
+++ src/common/guards/authenticated.guard.ts
@@ -2,7 +2,6 @@ import { ExecutionContext, Injectable, CanActivate } from '@nestjs/common';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-    public isEdits : Boolean = false;
 
     async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
